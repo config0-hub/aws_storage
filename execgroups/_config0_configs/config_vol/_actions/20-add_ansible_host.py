156c887b1a7ccb2714801ef7cdad04d0de34cd18
gary <gary>
2024-12-17 11:29:23 +0800

    rendered and updated 12-17-2024-11-28

---
+++ execgroups/_config0_configs/config_vol/_actions/20-add_ansible_host.py
@@ -0,0 +1,11 @@
+def default():
+
+    task = {'method': 'shelloutconfig',
+            'metadata': {'env_vars': [],
+                         'shelloutconfigs': ['config0-publish:::ansible::write_hosts']
+                         }
+
+            }
+
+    return task
+
